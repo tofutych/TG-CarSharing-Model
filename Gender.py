c1cd1b1174c8ee807fe23e94698f3ec788108506
Tofut <martirosov.davidka@mail.ru>
2021-04-01 19:26:54 +0300

    fix

--- Gender.py
+++ Gender.py
@@ -2,8 +2,8 @@ class Gender(object):
     genders = ['male', 'female']
 
     def __init__(self, gender):
-        if gender not in Gender.genders:
-            Gender.genders.append(gender)
+        if gender.lower() not in Gender.genders:
+            Gender.genders.append(gender.lower())
 
     @classmethod
     def all(cls):
