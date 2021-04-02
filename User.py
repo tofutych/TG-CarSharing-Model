cb299cd9a40810df686fb318d12f6712ce20304f
Tofut <martirosov.davidka@mail.ru>
2021-04-03 00:46:52 +0300

    __str__

--- User.py
+++ User.py
@@ -11,12 +11,14 @@ class User(object):
 
     def save(self):
         home = getcwd()
-        chdir('./users')
+        chdir("./users")
 
-        with open(f'{self.id}.json', 'w', encoding='utf-8') as f:
+        with open(f"{self.id}.json", "w", encoding="utf-8") as f:
             data = self.__dict__
-            data.pop('id')
+            data.pop("id")
             json.dump(data, f, ensure_ascii=False, indent=4)
 
         chdir(home)
 
+    def __str__(self):
+        return f"{self.name} {self.surname}, {self.bdate}, {self.gender.title()}\n{self.phone}\nНомер водительского: {self.license_number}\nСтаж вождения: {self.experience}\nТариф: {self.tariff}"
