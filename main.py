8c808c6ffa3cc54d5aa3fe1835a0d45048d52772
Tofut <martirosov.davidka@mail.ru>
2021-04-03 00:46:43 +0300

    cool menu

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ user_dict = {}
 
 def main_menu_kb():
     kb = types.InlineKeyboardMarkup(row_width=1)
-    kb.add(types.InlineKeyboardButton(
-        text='Информация о профиле', callback_data='profile'))
-    kb.add(types.InlineKeyboardButton(
-        text='Машины на карте', callback_data='cars'))
+    kb.add(
+        types.InlineKeyboardButton(text="Информация о профиле", callback_data="profile")
+    )
+    kb.add(types.InlineKeyboardButton(text="Машины на карте", callback_data="cars"))
+    return kb
 
+
+def back_kb():
+    kb = types.InlineKeyboardMarkup()
+    kb.add(types.InlineKeyboardButton(text="↩️", callback_data="main_menu"))
     return kb
 
 
@@ -32,12 +37,13 @@ def gender_kb():
 
 @bot.message_handler(commands=["start"])
 def start(message):
-    if f'{message.chat.id}.json' not in listdir('./users'):
+    if f"{message.chat.id}.json" not in listdir("./users"):
         msg = bot.reply_to(
-            message, f"Привет, {message.chat.first_name}!\nВведите ваше имя")
+            message, f"Привет, {message.chat.first_name}!\nВведите ваше имя"
+        )
         bot.register_next_step_handler(msg, process_name_step)
     else:
-        print('on tut')
+        print("on tut")
 
 
 def process_name_step(message):
@@ -45,11 +51,10 @@ def process_name_step(message):
         chat_id = message.chat.id
         user_dict[chat_id] = User(message.text)
         user_dict[chat_id].id = chat_id
-        msg = bot.send_message(
-            chat_id, 'Введите вашу фамилию')
+        msg = bot.send_message(chat_id, "Введите вашу фамилию")
         bot.register_next_step_handler(msg, process_surname_step)
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 def process_surname_step(message):
@@ -58,10 +63,13 @@ def process_surname_step(message):
         user = user_dict[chat_id]
         user.surname = message.text
         msg = bot.send_message(
-            chat_id, 'Выберите гендер\nЕсли его здесь нет, то напишите самостоятельно', reply_markup=gender_kb())
+            chat_id,
+            "Выберите гендер\nЕсли его здесь нет, то напишите самостоятельно",
+            reply_markup=gender_kb(),
+        )
         bot.register_next_step_handler(msg, process_gender_step)
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 def process_gender_step(message):
@@ -70,24 +78,27 @@ def process_gender_step(message):
         user = user_dict[chat_id]
         Gender(message.text)
         user.gender = message.text
-        msg = bot.send_message(chat_id, 'Введите дату рождения в формате ДД.ММ.ГГГГ',
-                               reply_markup=types.ReplyKeyboardRemove(selective=False))
+        msg = bot.send_message(
+            chat_id,
+            "Введите дату рождения в формате ДД.ММ.ГГГГ",
+            reply_markup=types.ReplyKeyboardRemove(selective=False),
+        )
         bot.register_next_step_handler(msg, process_bdate_step)
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 def process_bdate_step(message):
     try:
         chat_id = message.chat.id
         user = user_dict[chat_id]
-        user.age = message.text
+        user.bdate = message.text
 
-        msg = bot.send_message(chat_id, 'Ваш номер телефона')
+        msg = bot.send_message(chat_id, "Ваш номер телефона")
         bot.register_next_step_handler(msg, process_phone_step)
 
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 def process_phone_step(message):
@@ -96,11 +107,11 @@ def process_phone_step(message):
         user = user_dict[chat_id]
         user.phone = message.text
 
-        msg = bot.send_message(chat_id, 'Номер водительского удостоверения')
+        msg = bot.send_message(chat_id, "Номер водительского удостоверения")
         bot.register_next_step_handler(msg, process_license_number)
 
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 def process_license_number(message):
@@ -109,11 +120,11 @@ def process_license_number(message):
         user = user_dict[chat_id]
         user.license_number = message.text
 
-        msg = bot.send_message(chat_id, 'Стаж вождения')
+        msg = bot.send_message(chat_id, "Стаж вождения")
         bot.register_next_step_handler(msg, process_experience)
 
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 def process_experience(message):
@@ -123,22 +134,39 @@ def process_experience(message):
         user.experience = message.text
         user.save()
 
-        bot.reply_to(message, 'Регистрация звершена!')
-        bot.send_message(chat_id, 'Меню', reply_markup=main_menu_kb())
+        bot.reply_to(message, "Регистрация звершена!")
+        bot.send_message(chat_id, "Меню", reply_markup=main_menu_kb())
     except Exception as e:
-        bot.reply_to(message, 'ooops!!')
+        bot.reply_to(message, "ooops!!")
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
-    if call.data == "profile":
-        bot.answer_callback_query(call.id, 'Info')
+    if call.data == "main_menu":
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=f"Меню",
+            reply_markup=main_menu_kb(),
+        )
+    elif call.data == "profile":
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text=str(user_dict[call.message.chat.id]),
+            reply_markup=back_kb(),
+        )
     elif call.data == "cars":
-        bot.answer_callback_query(call.id, 'Cars')
+        bot.edit_message_text(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            text='молния маккуууиин',
+            reply_markup=back_kb(),
+        )
 
 
 bot.enable_save_next_step_handlers(delay=2)
 bot.load_next_step_handlers()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     bot.polling(none_stop=True)
